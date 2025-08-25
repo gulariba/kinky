66dc5a9ba83c09bddcb6ac5998d54eaeedc7d95f
Areeba Ahmed khan <159721838+gulariba@users.noreply.github.com>
2025-08-26 00:20:24 +0500

    Update sextoy.ts

--- src/app/data/sextoy.ts
+++ src/app/data/sextoy.ts
@@ -1,74 +1,23 @@
-const sexToyProducts = [
+export const sexToysProducts = [
   {
     id: 1,
     name: "Vibrating Dildo",
-    price: 90,
+    price: "90.00",
+    slug: "sex-toys",
     img: "/images/sex1.jpg",
-    category: "Sex Toys",
   },
   {
     id: 2,
     name: "Rabbit Vibrator",
-    price: 110,
+    price: "110.00",
+    slug: "sex-toys",
     img: "/images/sex2.jpg",
-    category: "Sex Toys",
   },
   {
     id: 3,
     name: "Clitoral Suction Toy",
-    price: 75,
+    price: "75.00",
+    slug: "sex-toys",
     img: "/images/sex3.jpg",
-    category: "Sex Toys",
-  },
-  {
-    id: 4,
-    name: "Glass Dildo",
-    price: 65,
-    img: "/images/sex4.jpg",
-    category: "Sex Toys",
-  },
-  {
-    id: 5,
-    name: "Butt Plug Set",
-    price: 80,
-    img: "/images/sex5.jpg",
-    category: "Sex Toys",
-  },
-  {
-    id: 6,
-    name: "Anal Beads",
-    price: 55,
-    img: "/images/sex6.jpg",
-    category: "Sex Toys",
-  },
-  {
-    id: 7,
-    name: "Remote Control Vibrator",
-    price: 130,
-    img: "/images/sex7.jpg",
-    category: "Sex Toys",
-  },
-  {
-    id: 8,
-    name: "Double Penetration Dildo",
-    price: 140,
-    img: "/images/sex8.jpg",
-    category: "Sex Toys",
-  },
-  {
-    id: 9,
-    name: "G-Spot Vibrator",
-    price: 115,
-    img: "/images/sex9.jpg",
-    category: "Sex Toys",
-  },
-  {
-    id: 10,
-    name: "Luxury Wand Massager",
-    price: 150,
-    img: "/images/sex10.jpg",
-    category: "Sex Toys",
   },
 ];
-
-export default sexToyProducts;
