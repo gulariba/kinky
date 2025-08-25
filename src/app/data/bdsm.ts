cffb10d042f9f885d0b1d1a15ab9525f63544324
Areeba Ahmed khan <159721838+gulariba@users.noreply.github.com>
2025-08-25 22:14:16 +0500

    Update bdsm.ts

--- src/app/data/bdsm.ts
+++ src/app/data/bdsm.ts
@@ -4,64 +4,73 @@ const bdsmProducts = [
   {
     id: 1,
     name: "Leather Corset",
+    category: "bdsm-wear",
     price: 120,
     img: "/images/bdsm1.jpg",
   },
   {
     id: 2,
     name: "Latex Bodysuit",
+    category: "bdsm-wear",
     price: 180,
     img: "/images/bdsm2.jpg",
   },
   {
     id: 3,
     name: "Dominatrix Whip",
+    category: "bdsm-wear",
     price: 60,
     img: "/images/bdsm3.jpg",
   },
   {
     id: 4,
     name: "Bondage Handcuffs",
+    category: "bdsm-wear",
     price: 45,
     img: "/images/bdsm4.jpg",
   },
   {
     id: 5,
     name: "Leather Paddle",
+    category: "bdsm-wear",
     price: 55,
     img: "/images/bdsm5.jpg",
   },
   {
     id: 6,
     name: "Latex Mask",
+    category: "bdsm-wear",
     price: 75,
     img: "/images/bdsm6.jpg",
   },
   {
     id: 7,
     name: "Bondage Rope Set",
+    category: "bdsm-wear",
     price: 40,
     img: "/images/bdsm7.jpg",
   },
   {
     id: 8,
     name: "Choker with Leash",
+    category: "bdsm-wear",
     price: 65,
     img: "/images/bdsm8.jpg",
   },
   {
     id: 9,
     name: "Faux Fur Handcuffs",
+    category: "bdsm-wear",
     price: 50,
     img: "/images/bdsm9.jpg",
   },
   {
     id: 10,
     name: "Gothic Collar Harness",
+    category: "bdsm-wear",
     price: 95,
     img: "/images/bdsm10.jpg",
   },
 ];
 
 export default bdsmProducts;
-
