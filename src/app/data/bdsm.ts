f3b616b5d8e52cf16b1a1bc38ccfbd51c66cb12f
Areeba Ahmed khan <159721838+gulariba@users.noreply.github.com>
2025-08-25 22:13:10 +0500

    Update bdsm.ts

--- src/app/data/bdsm.ts
+++ src/app/data/bdsm.ts
@@ -1,3 +1,5 @@
+// data/bdsm.ts
+
 const bdsmProducts = [
   {
     id: 1,
@@ -17,6 +19,49 @@ const bdsmProducts = [
     price: 60,
     img: "/images/bdsm3.jpg",
   },
+  {
+    id: 4,
+    name: "Bondage Handcuffs",
+    price: 45,
+    img: "/images/bdsm4.jpg",
+  },
+  {
+    id: 5,
+    name: "Leather Paddle",
+    price: 55,
+    img: "/images/bdsm5.jpg",
+  },
+  {
+    id: 6,
+    name: "Latex Mask",
+    price: 75,
+    img: "/images/bdsm6.jpg",
+  },
+  {
+    id: 7,
+    name: "Bondage Rope Set",
+    price: 40,
+    img: "/images/bdsm7.jpg",
+  },
+  {
+    id: 8,
+    name: "Choker with Leash",
+    price: 65,
+    img: "/images/bdsm8.jpg",
+  },
+  {
+    id: 9,
+    name: "Faux Fur Handcuffs",
+    price: 50,
+    img: "/images/bdsm9.jpg",
+  },
+  {
+    id: 10,
+    name: "Gothic Collar Harness",
+    price: 95,
+    img: "/images/bdsm10.jpg",
+  },
 ];
 
 export default bdsmProducts;
+
